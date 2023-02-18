02d55732bcd2d137fb7b139f2e93af4a6e02bd1b
Ummar Ikram <59392080+ummarikram@users.noreply.github.com>
2023-02-19 02:38:28 +0500

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s'
+    }
+  },
   logo: <span>SHIFA</span>,
   project: {
     link: 'https://github.com/SHIFA-HEALTHCARE',
