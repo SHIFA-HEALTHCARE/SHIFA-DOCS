61d69439ea2d93c6e7ed8772f5aad8e2deba4fb8
Ummar Ikram <59392080+ummarikram@users.noreply.github.com>
2023-02-19 02:43:04 +0500

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,6 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="SHIFA" />
+      <meta property="og:description" content="Decentralized Healthcare" />
+    </>
+  ),
   useNextSeoProps() {
     return {
       titleTemplate: '%s'
