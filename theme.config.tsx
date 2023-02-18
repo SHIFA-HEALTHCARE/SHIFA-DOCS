1274c8f075f749a63cdaea5c02f6aee80a58338c
Ummar Ikram <59392080+ummarikram@users.noreply.github.com>
2023-02-19 00:23:43 +0500

    Initial Setup

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>SHIFA</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/SHIFA-HEALTHCARE',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/SHIFA-HEALTHCARE/SHIFA-DOCS',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'SHIFA LLC',
   },
 }
 
